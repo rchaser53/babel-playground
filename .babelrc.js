cf50f26939ea435dff2d609ac5ad569761dfb45b
rchaser53 <tayoshizawa29@gmail.com>
2018-09-07 18:28:06 +0900

    setup for some features
    - proposal-decorators
    - object-rest-spread
    - transform-async-to-generator

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  "presets": ["@babel/preset-env", "@babel/preset-typescript"]
+  "presets": [
+    [ "@babel/preset-env", {
+      "targets": {
+        "node": "current"
+      }
+    }],
+    "@babel/preset-typescript"
+  ],
+  "plugins": [
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-async-to-generator",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+  ]
 }
--- src/test.vue
+++ src/test.vue
@@ -4,6 +4,23 @@
 
 <script lang="ts">
 import Vue from 'vue';
+// eslint-disable-next-line no-unused-vars
+import Component from 'vue-class-component';
 
-export default class Hoge extends Vue {}
+@Component({
+  async mounted() {
+    try {
+      const hoge = { a: 12, b: 33 };
+      const hogehoge = { ...hoge };
+      console.log(hogehoge);
+    } catch(err) {
+
+    }
+  }
+})
+export default class TestClass extends Vue {
+  hoge() {
+    return Promise.resolve();
+  }
+}
 </script>
