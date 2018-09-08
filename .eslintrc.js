4f1910c97a86cb461f3113890468412fb771825b
rchaser53 <tayoshizawa29@gmail.com>
2018-09-08 17:28:22 +0900

    .eslintrc => .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "parser": "typescript-eslint-parser",
+  // "parser": "babel-eslint",
+  "extends": [
+   "eslint:recommended"
+  ],
+  "env": {
+    "es6": true
+  },
+  "parserOptions": {
+    "sourceType": "module",
+  },
+}
